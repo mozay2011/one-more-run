422c7af6b1f99d8d10c03fd96f6f38f5d2e05cd0
mozay2011 <mohammedzayyanibrahim@gmail.com>
2026-08-18 19:52:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/one-more-run/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
